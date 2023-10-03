91f5cb4c613476c6cb5ad973e8c3d22079932390
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2023-10-02 23:02:11 -0300

    ci : add new scraped data functions  and eroror catch functions

--- scrapingData.js
+++ scrapingData.js
@@ -15,11 +15,17 @@ async function scrapeLogic() {
     await page.waitForSelector('#principal > div > div > div.col-md-3.sidebar-teee > div.filtro-fila.row.filtros-mobile.hidden-xs > div > div > div > ul > li > a');
     const anchorElement = await page.$('#principal > div > div > div.col-md-3.sidebar-teee > div.filtro-fila.row.filtros-mobile.hidden-xs > div > div > div > ul > li > a');
     await anchorElement.click();
-    await page.waitForSelector('div.items.area13[visible="true"][style*="display: block;"]');
+    const maxWaitTime = 30000;
+
+    try {
+      await Promise.race([
+        page.waitForSelector('div.items.area13'),
+        new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
+      ]);
 
     const visibleCardElements = await page.evaluate(() => {
       const visibleCards = [];
-      const elements = document.querySelectorAll('div.items.area13[visible="true"][style*="display: block;"]');
+      const elements = document.querySelectorAll('div.items.area13');
 
       function cutText(text, limit) {
         return (
@@ -56,12 +62,18 @@ async function scrapeLogic() {
               .querySelector(".label.label-estado")
               .textContent.match(/\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}/)[0]
           : "";
+        card.publicationDate = '';
         visibleCards.push(card);
       });
       return visibleCards;
     });
     await browser.close();
     return visibleCardElements;
+    } catch (error) {
+      console.error('Error:', error);
+      await browser.close();
+      return null;
+    }
 
   }
   
@@ -79,8 +91,14 @@ async function scrapeLogic() {
       visible: true,
     });
     await page.click(".typeahead__button > button");
-    await page.waitForSelector(".tag-busqueda");
+    const maxWaitTime = 30000;
 
+    try {
+      await Promise.race([
+        page.waitForSelector(".tag-busqueda"),
+        new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
+      ]);
+    
     //scraping
     const visibleCardElements = await page.evaluate(() => {
       const visibleCards = [];
@@ -123,6 +141,8 @@ async function scrapeLogic() {
               .querySelector(".label.label-estado")
               .textContent.match(/\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}/)[0]
           : "";
+        card.publicationDate = '';
+
 
         visibleCards.push(card);
       });
@@ -131,7 +151,11 @@ async function scrapeLogic() {
     });
     await browser.close();
     return visibleCardElements;
-
+    } catch (error) {
+      console.error('Error:', error);
+      await browser.close();
+      return null;
+    }
   }
 
   async function scrapEmpleosPublicos3() {
@@ -148,8 +172,13 @@ async function scrapeLogic() {
       visible: true,
     });
     await page.click(".typeahead__button > button");
-    await page.waitForSelector(".tag-busqueda");
+    const maxWaitTime = 30000;
 
+    try {
+      await Promise.race([
+        page.waitForSelector(".tag-busqueda"),
+        new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
+      ]);
     //scraping
     const visibleCardElements = await page.evaluate(() => {
       const visibleCards = [];
@@ -192,6 +221,8 @@ async function scrapeLogic() {
               .querySelector(".label.label-estado")
               .textContent.match(/\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2}/)[0]
           : "";
+        card.publicationDate = '';
+
 
         visibleCards.push(card);
       });
@@ -201,33 +232,130 @@ async function scrapeLogic() {
 
     await browser.close();
     return visibleCardElements;
-
+    } catch (error) {
+      console.error('Error:', error);
+      await browser.close();
+      return null;
+    }
   }
-  //scrap functions 
-  const scrapedData1 = await scrapEmpleosPublicos();
-  const scrapedData2 = await scrapEmpleosPublicos2();
-  const scrapedData3 = await scrapEmpleosPublicos3();
 
+async function scrapLinkdin(){
+    const browser = await puppeteer.launch({
+      headless: "new"
+    });
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(60000);
+    await page.goto("https://cl.linkedin.com/jobs/programador-empleos?position=1&pageNum=0");
+    
+     // Espera a que los elementos sean visibles
+     const maxWaitTime = 30000;
+
+     try {
+       await Promise.race([
+        page.waitForSelector('.jobs-search__results-list'),
+         new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
+       ]);
   
+  // Utiliza evaluate para extraer los datos de las tarjetas "base-card"
+  const data = await page.evaluate(() => {
+    const ul = document.querySelector('.jobs-search__results-list');
+    const cardList = ul.querySelectorAll('li div.base-card');
+    const cardData = [];
+    
+    cardList.forEach(card => {
+      const anchor = card.querySelector('a.base-card__full-link') || '';
+      const name = card.querySelector('.base-search-card__title') || '';
+      const enterprise = card.querySelector('a.hidden-nested-link') || '';
+      const location = card.querySelector('.job-search-card__location') || 'Desconocido';
+      let locationShort = '';
+      if (location.textContent.includes('Santiago') || location.textContent.includes('Chile')) {
+        locationShort = 'Santiago';
+      }
+      const details = card.querySelector('.result-benefits__text') || '';
+      const timeElement = card.querySelector('.job-search-card__listdate');
+      const publisher = 'https://www.linkedin.com';
+      const logoPublisher = 'https://th.bing.com/th/id/OIP.b5oDvUVU5UVN4cefTJGq3wHaHa?pid=ImgDet&rs=1';
+      let dateText = '';
+      
+      if (timeElement) {
+        const dateTime = timeElement.getAttribute('datetime');
+        dateText = dateTime.split("T")[0];
+      }
 
-  const uniqueNames = new Set();
+      if (anchor && name && enterprise && location) {
+        const locationText = location.textContent.trim().split(',')[0]; 
+
+        const cardInfo = {
+          name: name.textContent.trim(),
+          location: locationShort,
+          urlJob: anchor.getAttribute('href'),
+          publisher: publisher,
+          details: details ? details.textContent.trim() : '',
+          detailsCut: details ? details.textContent.trim() : '',
+          logoPublisher: logoPublisher,
+          enterprise: enterprise.textContent.trim(),
+          expirationDate: '',
+          publicationDate: dateText,
+        };
+        cardData.push(cardInfo);
+      }
+    });
+    
+    return cardData;
+  });
+
+  await browser.close();
+  return data
+  } catch (error) {
+    console.error('Error:', error);
+    await browser.close();
+    return null;
+  }
+}
 
   function addUniqueObjects(dataArray) {
     dataArray.forEach((obj) => {
-      if (!uniqueNames.has(obj.name)) {
-        uniqueNames.add(obj.name);
+      const isDuplicate = dataEmpleosPublicos.some((existingObj) => {
+        return existingObj.name === obj.name && existingObj.publisher === obj.publisher;
+      });
+  
+      if (!isDuplicate) {
+        dataEmpleosPublicos.push(obj);
+      }
+    });
+  }
+  
+  let scrapedData1; let scrapedData2;let scrapedData3;let scrapedData4;
+  const dataEmpleosPublicos = [];
+
+  if ((scrapedData1 = await scrapEmpleosPublicos()) !== null) {
+    addUniqueObjects(scrapedData1);
+  }
+  if ((scrapedData2 = await scrapEmpleosPublicos2()) !== null) {
+    addUniqueObjects(scrapedData2);
+  }
+  if ((scrapedData3 = await scrapEmpleosPublicos3()) !== null) {
+    addUniqueObjects(scrapedData3);
+  }
+
+  if ((scrapedData4 = await scrapLinkdin()) !== null) {
+    addUniqueObjects(scrapedData4);
+  }
+  
+  function addUniqueObjects(dataArray) {
+    dataArray.forEach((obj) => {
+      const isDuplicate = dataEmpleosPublicos.some((existingObj) => {
+        return existingObj.name === obj.name && existingObj.publisher === obj.publisher;
+      });
+  
+      if (!isDuplicate) {
         dataEmpleosPublicos.push(obj);
       }
     });
   }
 
-  const dataEmpleosPublicos = [];
-
-  addUniqueObjects(scrapedData1);
-  addUniqueObjects(scrapedData2);
-  addUniqueObjects(scrapedData3);
-
   return dataEmpleosPublicos;
+
 }
 
 module.exports = {
