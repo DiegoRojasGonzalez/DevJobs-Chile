db7e1e1e126e8d05f48b4bb85f7b5c46776f094a
Diego Ignacio Rojas Gonzalez <gonzalez.rojas.ignacio.diego@gmail.com>
2023-10-04 13:42:30 -0300

    feat  : change scrpaingdata.js new scrap data

--- scrapingData.js
+++ scrapingData.js
@@ -77,7 +77,6 @@ async function scrapeLogic() {
 
   }
   
-  
   async function scrapEmpleosPublicos2() {
     const browser = await puppeteer.launch({
       headless: "new"
@@ -240,79 +239,218 @@ async function scrapeLogic() {
     }
   }
 
-async function scrapLinkdin(){
+  async function scrapLinkdin(){
+      const browser = await puppeteer.launch({
+        headless: "new"
+      });
+      const page = await browser.newPage();
+      await page.setDefaultNavigationTimeout(60000);
+      await page.goto("https://cl.linkedin.com/jobs/programador-empleos?position=1&pageNum=0");
+      
+      // Espera a que los elementos sean visibles
+      const maxWaitTime = 30000;
+
+      try {
+        await Promise.race([
+          page.waitForSelector('.jobs-search__results-list'),
+          new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
+        ]);
+    
+    // Utiliza evaluate para extraer los datos de las tarjetas "base-card"
+    const data = await page.evaluate(() => {
+      const ul = document.querySelector('.jobs-search__results-list');
+      const cardList = ul.querySelectorAll('li div.base-card');
+      const cardData = [];
+      
+      cardList.forEach(card => {
+        const anchor = card.querySelector('a.base-card__full-link') || '';
+        const name = card.querySelector('.base-search-card__title') || '';
+        const enterprise = card.querySelector('a.hidden-nested-link') || '';
+        const location = card.querySelector('.job-search-card__location') || 'Desconocido';
+        let locationShort = '';
+        if (location.textContent.includes('Santiago') || location.textContent.includes('Chile')) {
+          locationShort = 'Santiago';
+        }
+        const details = card.querySelector('.result-benefits__text') || '';
+        const timeElement = card.querySelector('.job-search-card__listdate');
+        const publisher = 'https://www.linkedin.com';
+        const logoPublisher = 'https://th.bing.com/th/id/OIP.b5oDvUVU5UVN4cefTJGq3wHaHa?pid=ImgDet&rs=1';
+        let dateText = '';
+        
+        if (timeElement) {
+          const dateTime = timeElement.getAttribute('datetime');
+          dateText = dateTime.split("T")[0];
+        }
+
+        if (anchor && name && enterprise && location) {
+          const locationText = location.textContent.trim().split(',')[0]; 
+
+          const cardInfo = {
+            name: name.textContent.trim(),
+            location: locationShort,
+            urlJob: anchor.getAttribute('href'),
+            publisher: publisher,
+            details: details ? details.textContent.trim() : '',
+            detailsCut: details ? details.textContent.trim() : '',
+            logoPublisher: logoPublisher,
+            enterprise: enterprise.textContent.trim(),
+            expirationDate: '',
+            publicationDate: dateText,
+          };
+          cardData.push(cardInfo);
+        }
+      });
+      
+      return cardData;
+    });
+
+    await browser.close();
+    return data
+    } catch (error) {
+      console.error('Error:', error);
+      await browser.close();
+      return null;
+    }
+  }
+
+  async function scrapCompuTrabajo(){
+  
     const browser = await puppeteer.launch({
       headless: "new"
     });
+  
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(60000);
-    await page.goto("https://cl.linkedin.com/jobs/programador-empleos?position=1&pageNum=0");
-    
-     // Espera a que los elementos sean visibles
-     const maxWaitTime = 30000;
+    await page.goto("https://cl.computrabajo.com/trabajo-de-programador");  
+    const maxWaitTime = 30000;
 
-     try {
-       await Promise.race([
-        page.waitForSelector('.jobs-search__results-list'),
-         new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
-       ]);
-  
-  // Utiliza evaluate para extraer los datos de las tarjetas "base-card"
-  const data = await page.evaluate(() => {
-    const ul = document.querySelector('.jobs-search__results-list');
-    const cardList = ul.querySelectorAll('li div.base-card');
-    const cardData = [];
-    
-    cardList.forEach(card => {
-      const anchor = card.querySelector('a.base-card__full-link') || '';
-      const name = card.querySelector('.base-search-card__title') || '';
-      const enterprise = card.querySelector('a.hidden-nested-link') || '';
-      const location = card.querySelector('.job-search-card__location') || 'Desconocido';
-      let locationShort = '';
-      if (location.textContent.includes('Santiago') || location.textContent.includes('Chile')) {
-        locationShort = 'Santiago';
-      }
-      const details = card.querySelector('.result-benefits__text') || '';
-      const timeElement = card.querySelector('.job-search-card__listdate');
-      const publisher = 'https://www.linkedin.com';
-      const logoPublisher = 'https://th.bing.com/th/id/OIP.b5oDvUVU5UVN4cefTJGq3wHaHa?pid=ImgDet&rs=1';
-      let dateText = '';
+    try {
+      await Promise.race([
+      page.waitForSelector('.box_offer'),
       
-      if (timeElement) {
-        const dateTime = timeElement.getAttribute('datetime');
-        dateText = dateTime.split("T")[0];
-      }
+        new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
+      ]);
+      const articles = await page.$$('.box_offer');
 
-      if (anchor && name && enterprise && location) {
-        const locationText = location.textContent.trim().split(',')[0]; 
-
-        const cardInfo = {
-          name: name.textContent.trim(),
-          location: locationShort,
-          urlJob: anchor.getAttribute('href'),
-          publisher: publisher,
-          details: details ? details.textContent.trim() : '',
-          detailsCut: details ? details.textContent.trim() : '',
-          logoPublisher: logoPublisher,
-          enterprise: enterprise.textContent.trim(),
-          expirationDate: '',
-          publicationDate: dateText,
+      const articleData = [];
+  /** */
+      for (const article of articles) {
+        const name = await article.$eval('h2.fs18 a', (a) => a.textContent.trim() || '');
+        let location = await article.$eval('p.fs16.fc_base.mt5.mb5', (p) => {
+          const text = p.textContent.trim();
+          const locationParts = text.split('\n'); // Dividir el texto en líneas
+          const lastLine = locationParts[locationParts.length - 1].trim(); // Obtener la última línea (ubicación) y eliminar espacios en blanco adicionales
+          if (lastLine.includes('Santiago')) {
+            return 'Santiago';
+          } else {
+            return lastLine;
+          }
+        });
+        const urlJob = await article.$eval('h2.fs18 a', (a) => a.getAttribute('href') || '');
+        const publisher = 'https://cl.computrabajo.com';
+        const details = await article.$eval('div.list_dot.mb5.mrB', (div) => div.textContent.trim().replace(/\s+/g, ' ') || '');
+        const detailsCut = '';
+        const logoPublisher = 'https://media-exp2.licdn.com/dms/image/C4E0BAQG_OEsCOrplZA/company-logo_200_200/0/1639640793918?e=2147483647&v=beta&t=rBg-umDIGY6_ALxG6yUN4g6YzNLNzAvcNc8JFkG0bvo';
+        const company = await article.$eval('a.fc_base:not(.js-o-link) ', (a) => a.textContent.trim().replace(/\s+/g, ' ') || '');
+        const enterprise = company.includes('Postular') ? 'Desconocido' : company; 
+        const expirationDate ='';
+        const publicationDate ='';
+        const articleInfo = {
+          name,
+          location,
+          urlJob,
+          publisher,
+          details,
+          detailsCut,
+          logoPublisher,
+          enterprise,
+          expirationDate,
+          publicationDate
         };
-        cardData.push(cardInfo);
+        
+        articleData.push(articleInfo);
       }
-    });
-    
-    return cardData;
-  });
+      await browser.close();
+      return articleData;
+      
 
-  await browser.close();
-  return data
-  } catch (error) {
-    console.error('Error:', error);
-    await browser.close();
-    return null;
+    } catch (error) {
+      console.error('Error:', error);
+      await browser.close();
+      return null;
+    }
+    
   }
-}
+
+  async function scrapCompuTrabajo2(){
+    const browser = await puppeteer.launch({headless: false});
+  
+
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(60000);
+    await page.goto("https://cl.computrabajo.com/trabajo-de-programador?p=2");  
+    const maxWaitTime = 30000;
+
+    try {
+      await Promise.race([
+      page.waitForSelector('.box_offer'),
+      
+        new Promise((_, reject) => setTimeout(() => reject('Tiempo de espera agotado'), maxWaitTime))
+      ]);
+      const articles = await page.$$('.box_offer');
+
+      const articleData = [];
+  /** */
+      for (const article of articles) {
+        const name = await article.$eval('h2.fs18 a', (a) => a.textContent.trim() || '');
+        let location = await article.$eval('p.fs16.fc_base.mt5.mb5', (p) => {
+          const text = p.textContent.trim();
+          const locationParts = text.split('\n'); // Dividir el texto en líneas
+          const lastLine = locationParts[locationParts.length - 1].trim(); // Obtener la última línea (ubicación) y eliminar espacios en blanco adicionales
+          if (lastLine.includes('Santiago')) {
+            return 'Santiago';
+          } else {
+            return lastLine;
+          }
+        });
+        const urlJob = await article.$eval('h2.fs18 a', (a) => a.getAttribute('href') || '');
+        const publisher = 'https://cl.computrabajo.com';
+        const details = await article.$eval('div.list_dot.mb5.mrB', (div) => div.textContent.trim().replace(/\s+/g, ' ') || '');
+        const detailsCut = '';
+        const logoPublisher = 'https://media-exp2.licdn.com/dms/image/C4E0BAQG_OEsCOrplZA/company-logo_200_200/0/1639640793918?e=2147483647&v=beta&t=rBg-umDIGY6_ALxG6yUN4g6YzNLNzAvcNc8JFkG0bvo';
+        const company = await article.$eval('a.fc_base:not(.js-o-link) ', (a) => a.textContent.trim().replace(/\s+/g, ' ') || '');
+        const enterprise = company.includes('Postular') ? 'Desconocido' : company; 
+        const expirationDate ='';
+        const publicationDate ='';
+        const articleInfo = {
+          name,
+          location,
+          urlJob,
+          publisher,
+          details,
+          detailsCut,
+          logoPublisher,
+          enterprise,
+          expirationDate,
+          publicationDate
+        };
+        
+        articleData.push(articleInfo);
+      }
+      await browser.close();
+      return articleData;
+      
+
+    } catch (error) {
+      console.error('Error:', error);
+      await browser.close();
+      return null;
+    }
+    
+  }
+
+
+
 
   function addUniqueObjects(dataArray) {
     dataArray.forEach((obj) => {
@@ -326,7 +464,7 @@ async function scrapLinkdin(){
     });
   }
   
-  let scrapedData1; let scrapedData2;let scrapedData3;let scrapedData4;
+  let scrapedData1; let scrapedData2;let scrapedData3;let scrapedData4;let scrapedData5;let scrapedData6;
   const dataEmpleosPublicos = [];
 
   if ((scrapedData1 = await scrapEmpleosPublicos()) !== null) {
@@ -345,10 +483,12 @@ async function scrapLinkdin(){
     addUniqueObjects(scrapedData4);
   }
   
-  console.log("DATA 2:",scrapedData2)
-  console.log("DATA 3:",scrapedData3)
-  console.log("DATA 4:",scrapedData4)
-
+  if ((scrapedData5 = await scrapCompuTrabajo()) !== null) {
+    addUniqueObjects(scrapedData5);
+  }
+  if ((scrapedData6 = await scrapCompuTrabajo2()) !== null) {
+    addUniqueObjects(scrapedData6);
+  }
 
   function addUniqueObjects(dataArray) {
     dataArray.forEach((obj) => {
